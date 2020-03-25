df79b81f0fa1e3022ba8dc1867a254f12d4d79a1
Mrbabibo <Mrbabibo@163.com>
2020-03-25 20:32:00 +0800

    Add files via upload

---
+++ precache-manifest.dcd4b8daf40d444ff50c4640d739d814.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "b97b516464628e17fec8",
+    "url": "js/chunk-vendors.5b563517.js"
+  },
+  {
+    "revision": "7a3452b6b20c620dac18",
+    "url": "js/app.d96ef0fc.js"
+  },
+  {
+    "revision": "4d98673c071d1389751f46cd08db0f8e",
+    "url": "index.html"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "b97b516464628e17fec8",
+    "url": "css/chunk-vendors.728eb7d9.css"
+  },
+  {
+    "revision": "7a3452b6b20c620dac18",
+    "url": "css/app.f4dbbcfd.css"
+  },
+  {
+    "revision": "07d5a8f9ac641b2547ea07afa2f9b039",
+    "url": "config/index.js"
+  },
+  {
+    "revision": "e651c448b3dbab4e2dd96aca5281b701",
+    "url": "config/echarts.min.js"
+  }
+];
